1938dbe2f56169199b442cb2dca7ad9223075fa2
Morten Brøns-Pedersen <f@ntast.dk>
2015-07-09 13:00:42 +0200

    remember -x flag

--- 50check-pkgs.sh
+++ 50check-pkgs.sh
@@ -5,6 +5,7 @@ prompt_install check-pkgs check-pkgs
 
 run mkdir -p ~/bin
 run wget https://raw.githubusercontent.com/br0ns/check-pkgs/master/check-pkgs -O ~/bin/check-pkgs
+run chmod +x ~/bin/check-pkgs
 run sudo tee /etc/apt/apt.conf.d/99check-pkgs <<EOF
 DPkg::Post-Invoke {"$HOME/bin/check-pkgs";};
 EOF
--- 50net.sh
+++ 50net.sh
@@ -6,4 +6,5 @@ prompt_install net
 run mkdir -p ~/bin
 for f in net _net_bash_completion ; do
     run wget https://raw.githubusercontent.com/br0ns/net/master/$f -O ~/bin/$f
+    run chmod +x ~/bin/$f
 done
