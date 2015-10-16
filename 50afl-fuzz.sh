ac0ff25ef7ba081ae6a591e3081e836b620fc179
Morten Brøns-Pedersen <f@ntast.dk>
2015-10-16 10:09:31 +0200

    50afl-fuzz.sh now also builds qemu mode

--- 50afl-fuzz.sh
+++ 50afl-fuzz.sh
@@ -3,10 +3,15 @@ source "$(dirname "$0")/bootstrap"
 
 prompt_install afl-fuzz "American Fuzzy Lop"
 
+require libtool-bin automake autoconf bison libglib2.0-dev
+
 goto_tempdir
 
 run wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
 run tar xzf afl-latest.tgz
 run cd afl-*
 run make
+cd qemu_mode
+run ./build_qemu_support.sh
+cd ..
 run sudo make install
