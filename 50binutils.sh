839319e0eb0fd8f27c53ddbde7401161d18542d3
Morten Brøns-Pedersen <f@ntast.dk>
2015-10-16 10:11:47 +0200

    updated 50binutils.sh

--- 50binutils.sh
+++ 50binutils.sh
@@ -8,18 +8,33 @@ goto_tempdir
 
 require gcc make texinfo
 
-run wget http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz.sig
-run gpg --verify binutils-2.25.tar.gz.sig || exit 1
+PARALLELISM=8
+BINUTILS_VERSION=2.25
 
-run tar zxfv binutils-2.25.tar.gz
+run wget http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz
+run wget http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz.sig
+run gpg --verify binutils-$BINUTILS_VERSION.tar.gz.sig || exit 1
+run tar zxfv binutils-$BINUTILS_VERSION.tar.gz
 
-for TARGET in i386 x86_64 aarch64 alpha avr arm mips msp430 powerpc m68k sparc vax; do
+function PREFIX () {
+    echo /opt/cross-$1
+}
+
+function TARGET () {
+    echo $1-unknown-linux-gnu
+}
+
+for ARCH in i386 x86_64 aarch64 alpha avr arm mips msp430 powerpc m68k sparc vax; do
+    PREFIX=$(PREFIX $ARCH)
+    TARGET=$(TARGET $ARCH)
+
     run rm -rf build
     run mkdir build
     run pushd build
-    run ../binutils-2.25/configure --prefix=/opt/binutils-$TARGET \
-                               --target=$TARGET-unknown-linux-gnu --disable-static --disable-multilib \
-                               --disable-werror --disable-nls && \
-        run make -j5 && sudo make install
+    run ../binutils-$BINUTILS_VERSION/configure --prefix=$PREFIX \
+        --target=$TARGET --disable-nls --disable-multilib --disable-static \
+        --disable-werror
+    run make -j$PARALLELISM
+    run sudo make install
     run popd
 done
