05afdb932a4b342eb0ff5b8a059a39dfa662f41b
Morten Brøns-Pedersen <f@ntast.dk>
2015-10-20 12:01:27 +0200

    bugfix in 50cryptominisat.sh

--- 50cryptominisat.sh
+++ 50cryptominisat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source "$(dirname "$0")/bs.sh"
 
-prompt_install cryptominisat_simple CryptoMiniSat
+prompt_install cryptominisat4_simple CryptoMiniSat
 
 # vim-common supplies `xxd'
 require build-essential cmake vim-common
@@ -9,7 +9,7 @@ require build-essential cmake vim-common
 goto_tempdir
 
 git clone git@github.com:msoos/cryptominisat.git .
-mkdir build
+mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/cryptominisat ..
 make -j4
