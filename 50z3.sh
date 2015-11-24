d7bddf6de7f12600fc883a49c7bab1f224b5ab1a
Morten Brøns-Pedersen <f@ntast.dk>
2015-11-24 16:59:26 +0100

    sudo

--- 50z3.sh
+++ 50z3.sh
@@ -9,7 +9,7 @@ sudo rm -rf /opt/z3
 sudo mkdir /opt/z3
 
 git clone git@github.com:Z3Prover/z3.git .
-python scripts/mk_make.py --prefix=/opt/z3
+sudo python scripts/mk_make.py --prefix=/opt/z3
 cd build
 sudo make
 sudo make install
