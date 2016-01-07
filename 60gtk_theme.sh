1f555b83b95f9d3167e3a4e3009af1ce4f1e1dfd
Morten Brøns-Pedersen <f@ntast.dk>
2016-01-07 12:53:28 +0100

    write .config/Trolltech.conf

--- 60gtk_theme.sh
+++ 60gtk_theme.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 source "$(dirname "$0")/bs.sh"
 
+cat > .config/Trolltech.conf <<EOF
+[Qt]
+style=GTK+
+EOF
+
 [ -e /usr/share/themes/FlatStudio/ ] && exit 0
 
 prompt_step "FlatStudio GTK theme is not installed; Download and install now?" "y"
@@ -9,8 +14,3 @@ goto_tempdir
 
 wget http://gnome-look.org/CONTENT/content-files/154296-FlatStudio-1.03.tar.gz -O flatstudio.tgz
 sudo tar xfv flatstudio.tgz -C /usr/share/themes/
-
-.config/Trolltech.conf <<EOF
-[Qt]
-style=GTK+
-EOF
