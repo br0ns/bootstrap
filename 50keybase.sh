967c5db55561dfad0adcb969d9cefb6e7bace695
Morten Brøns-Pedersen <f@ntast.dk>
2016-01-21 23:12:21 +0100

    install script for keybase

---
+++ 50keybase.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source "$(dirname "$0")/bs.sh"
+
+prompt_install keybase Keybase
+
+goto_tempdir
+
+curl -O https://dist.keybase.io/linux/deb/keybase-latest-amd64.deb
+sudo dpkg -i keybase-latest-amd64.deb
