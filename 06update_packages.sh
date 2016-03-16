f50824503cfb2f6063ff729355c5b7055cc6d903
Morten Brøns-Pedersen <f@ntast.dk>
2016-03-16 13:18:09 +0100

    06update_packages: change default answer to no if last update was less than 5mins ago

--- 06update_packages.sh
+++ 06update_packages.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 source "$(dirname "$0")/bs.sh"
 
-prompt_step "Update packages now?" "y"
+since_last=$(($(date +%s)-$(stat -c %Y /var/cache/apt/)))
+# Ten minutes
+if [ $since_last -gt 600 ] ; then
+    default=y
+else
+    default=n
+fi
+
+prompt_step "Update packages now?" $default
 
 apt-get update
 apt-get upgrade
