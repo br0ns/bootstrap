60924977134889abd9dddd9f5b428c1200bd732c
Morten Brøns-Pedersen <f@ntast.dk>
2016-03-16 13:21:47 +0100

    bugfix: 60firefox now works when firefox is not installed

--- 60firefox.sh
+++ 60firefox.sh
@@ -3,9 +3,11 @@ source "$(dirname "$0")/bs.sh"
 
 INFO Installing \`userContent.css\`
 
+shopt -s nullglob
+
 for d in .mozilla/firefox/*/chrome ; do
     cat > $d/userContent.css <<EOF
-input, textarea {
+input, textarea, select {
   color: black !important;
   background-color: white !important;
 }
