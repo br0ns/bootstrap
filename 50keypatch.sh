f0a36cda278a2b31074da6c826b8e976833b2c6c
Morten Brøns-Pedersen <f@ntast.dk>
2016-10-12 09:23:04 +0200

    dependencies for keypatch

--- 50keypatch.sh
+++ 50keypatch.sh
@@ -8,7 +8,22 @@ if [ -z $IDAPATH ] ; then
     exit
 fi
 
-prompt_install $IDAPATH/plugins/keypatch.py IdaRef
+prompt_install $IDAPATH/plugins/keypatch.py Keypatch
 
 cd $IDAPATH/plugins/
 sudo wget https://raw.githubusercontent.com/keystone-engine/keypatch/master/keypatch.py
+
+require gcc libc6-dev cmake
+
+goto_tempdir
+
+git clone https://github.com/keystone-engine/keystone.git
+cd keystone
+mkdir build
+cd build
+../make-share.sh lib32 lib_only
+cd ..
+
+sudo cp -r bindings/python/keystone $IDAPATH/python/
+sudo cp -r /usr/lib/python2.7/distutils $IDAPATH/python/
+sudo cp build/llvm/lib/libkeystone.so.* $IDAPATH/python/keystone/
