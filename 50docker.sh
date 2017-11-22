3d1132a9c398b6176473433610495243d6d758c8
Morten Brøns-Pedersen <f@ntast.dk>
2017-11-22 19:50:05 +0100

    install Docker via https://get.docker.com

--- 50docker.sh
+++ 50docker.sh
@@ -3,32 +3,11 @@ source "$(dirname "$0")/bs.sh"
 
 prompt_install docker Docker
 
-require apt-transport-https
+require curl
 
 goto_tempdir
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
-     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-if ! grep dockerproject /etc/apt/sources.list ; then
-    if grep -qsi "debian" /etc/issue ; then
-        sudo tee -a /etc/apt/sources.list <<EOF
-
-deb https://apt.dockerproject.org/repo debian-jessie main
-EOF
-    fi
-
-    if grep -qsi "ubuntu 14" /etc/issue ; then
-        sudo tee -a /etc/apt/sources.list <<EOF
-
-deb https://apt.dockerproject.org/repo ubuntu-trusty main
-EOF
-    fi
-
-    sudo apt-get update
-fi
-
-sudo apt-get install docker-engine
+curl -fsSL https://get.docker.com | sudo sh
 
 sudo groupadd docker || true
 sudo usermod -aG docker $USER
