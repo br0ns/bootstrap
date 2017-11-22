0cd028b1f64038bb9ac97f86a7eafee31e5ac643
Morten Brøns-Pedersen <f@ntast.dk>
2017-11-22 19:50:44 +0100

    pass --recommends flag to check-pkgs

--- 99check_pkgs.sh
+++ 99check_pkgs.sh
@@ -3,7 +3,7 @@ source "$(dirname "$0")/bs.sh"
 
 if [ ! -e /etc/apt/apt.conf.d/99check-pkgs ] ; then
     sudo tee /etc/apt/apt.conf.d/99check-pkgs <<EOF
-DPkg::Post-Invoke {"$HOME/bin/check-pkgs";};
+DPkg::Post-Invoke {"$HOME/bin/check-pkgs --recommends";};
 EOF
 fi
 
@@ -12,4 +12,4 @@ prompt_step "Run \`check-pkgs' now?" "y"
 # INFO Updating Linux kernel headers in case kernel modules are installed
 # apt-get -y install linux-headers-$(uname -r)
 
-check-pkgs
+check-pkgs --recommends
