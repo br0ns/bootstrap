9001cf324806f1e1808815aeb97590f5141331a5
Morten Brøns-Pedersen <f@ntast.dk>
2017-12-05 22:07:17 +0100

    updated 50gdb

--- 50gdb.sh
+++ 50gdb.sh
@@ -4,14 +4,14 @@ source "$(dirname "$0")/bs.sh"
 prompt_install gdb GDB
 
 # List from `apt-rdepends --build-depends --follow=DEPENDS gdb`
-require autoconf bison bzip2 cdbs debhelper dejagnu flex flex-old gettext \
+require autoconf bison bzip2 cdbs debhelper dejagnu flex-old g++ gettext \
         gobjc libbabeltrace-ctf-dev libbabeltrace-dev libexpat1-dev \
         liblzma-dev libncurses5-dev libreadline-dev libtool lsb-release procps \
         python2.7-dev texinfo texlive-base zlib1g-dev
 
 goto_tempdir
 
-git clone git://anonscm.debian.org/pkg-gdb/gdb.git .
+git clone git://sourceware.org/git/binutils-gdb.git .
 ./configure --with-python=python2
 make
 
